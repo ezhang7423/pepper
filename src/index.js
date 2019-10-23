08e737b1ee5543b64ad759a4683fbd33f3350c87
Rowan-James Tran <rowanjames.tran@gmail.com>
2019-10-23 00:09:29 -0700

    Use Octokit API for requests, handle pagination. Closes #8

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const Octokit = require("@octokit/rest");
+
 const store = require("./store.js")
 
-if (process.argv.length == 5){
-const username = process.argv[2];
-const token = process.argv[3];
-const org = process.argv[4];
+if (process.argv.length == 5) {
+    const username = process.argv[2];
+    const token = process.argv[3];
+    const org = process.argv[4];
 
-let orgURL = "https://api.github.com/orgs/" + org + "/members";
-axios.get(orgURL, {
-    auth: {
-        username: username,
-        password: token
-    }
-})
-    .then(res => {
-        res.data.forEach((user) => {
+    const octokit = new Octokit({
+        auth: {
+            username: username,
+            password: token,
+            async on2fa() {
+                return prompt("2FA code: ");
+            }
+        }
+    });
+
+    octokit.paginate("GET /orgs/:org/members", {
+        org: org
+    })
+    .then((users) => {
+        users.forEach((user) => {
             store.writeContributions(user.html_url);
         });
     })
     .catch(err => {
         console.log(err);
     });
+} else {
+    console.log("USAGE: node index.js username token org-name")
 }
-else console.log("USAGE: node index.js username token org-name")
