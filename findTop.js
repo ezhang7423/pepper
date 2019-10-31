b09a6ba6725990237c96c3c2d0146207be0119ed
ezhang7423 <ezhang7423@student.palomar.edu>
2019-10-30 20:32:12 -0700

    finished topOutput

--- findTop.js
+++ findTop.js
@@ -4,8 +4,10 @@ const path = require('path')
 
 function printContribuitions(commits){
     keys = Object.keys(commits)
+    totals = reduceToTuple(commits)
         for (i = 0; i < keys.length; i++){
             console.log(keys[i])
+            console.log("total contribuitions:", totals[i][1])
             individualCommits = commits[keys[i]]
             indivKeys = Object.keys(individualCommits)
             for (j = 0; j < indivKeys.length; j++){
@@ -37,28 +39,47 @@ function filterToTop(tupleCommits, topAmt){
         top.push(tupleCommits[x])
     }
     top = top.sort((a, b) => a[1] - b[1])
-    for (x = 0; x < tupleCommits.length; x++){
+    for (x = topAmt; x < tupleCommits.length-topAmt; x++){
         if (tupleCommits[x][1] > top[0][1]){
             top[0] = tupleCommits[x]
+            top = top.sort((a, b) => a[1] - b[1])
         }
-        top = top.sort((a, b) => a[1] - b[1])
+    }
+    top.reverse();
+    temp = []
+    for (item of top){
+        temp.push(item[0])
     }
     return top
 
 }
-if (process.argv.length == 3){
+
+function expandTuple(top10, commits){
+    top = {}
+    for (i = 0; i < top10.length; i++){
+
+        top[top10[i][0]] = {};
+        top[top10[i][0]] = commits[top10[i][0]]
+    }
+    return top
+}
+if (process.argv.length == 4){
+    
     orgName = process.argv[2]
     filePath = path.join(__dirname,"data", orgName)
     fs.readFile(filePath, (err, data) =>{
         if (err) console.log(err)
         commits = JSON.parse(data)
-        top10 = reduceToTuple(commits)
-        top10 = filterToTop(top10, 5)
-        console.log(top10)
-        //expand top10 to actual contributions
-        printContribuitions(commits)
+        if (process.argv[3] > Object.keys(commits).length){
+            amt = Object.keys(commits).length
+        }
+        else{
+            amt = process.argv[3]
+        }
+        top10 = expandTuple(filterToTop(reduceToTuple(commits),amt), commits)
+        printContribuitions(top10)
     })
 }
 else{
-    console.log("USAGE: outputData.js orgName.json")
+    console.log("USAGE: outputData.js orgName.json amtOfPeople")
 }
