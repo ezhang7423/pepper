1285f3d300e2225e96ef93de334704beec40378b
ezhang7423 <ezhang7423@student.palomar.edu>
2019-12-02 12:05:07 -0800

    restructure project for backend

---
+++ public/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('./index.html  ')
+})
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
