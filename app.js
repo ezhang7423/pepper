ad69a90eae525a90b3efcb785ac931173195f2c1
ezhang7423 <ezhang7423@student.palomar.edu>
2019-12-02 12:08:22 -0800

    create serve cmd

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-    res.send('./index.html  ')
+    res.sendFile('index.html')
 })
 
 
